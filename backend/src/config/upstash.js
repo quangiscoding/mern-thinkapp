f8fd1f750c368a0202ef0c20f8a2244b9fff45dd
minhquang1610 <quangnguyen2005gt08@gmail.com>
2026-01-25 08:22:34 +0700

    Fix page: fix HomePage RateLimitedUI bug by reordering middleware in server.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -9,7 +9,7 @@ const rateLimit = new Ratelimit({
     url: process.env.UPSTASH_REDIS_REST_URL,
     token: process.env.UPSTASH_REDIS_REST_TOKEN,
   }),
-  limiter: Ratelimit.slidingWindow(3, "1 s"),
+  limiter: Ratelimit.slidingWindow(5, "1 s"),
 });
 
 export default rateLimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
@@ -10,44 +11,40 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+const NODE_ENV = process.env.NODE_ENV;
 
-/*
- * Enable cors for frontend
- */
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
-/*
- * Parse incoming JSON bodies
- */
+const frontendDist =
+  NODE_ENV === "production"
+    ? path.join(__dirname, "../frontend", "dist")
+    : null;
+
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-/*
- * Prints every method, body, and its url when a request sent
- */
-app.use((req, _, next) => {
-  console.log(req.method, req.body, req.url);
-  next();
-});
-/*
- * Rate limiting
- */
-app.use(rateLimiter);
-/**
- * Routes
- */
+
+if (NODE_ENV === "production") {
+  console.log("Serving frontend from:", frontendDist);
+  app.use(express.static(frontendDist));
+}
+// app.use((req, _, next) => {
+//   console.log(req.method, req.body, req.url);
+//   next();
+// });
+app.use("/api", rateLimiter);
 app.use("/api/notes", notesRoutes);
-/**
- * Global error handler (must be last)
- */
 app.use((err, req, res, next) => {
   console.error("ERROR:", err);
   res.status(err.status || 500).json({
     message: err.message || "Internal server error!",
   });
 });
-/*
+
+if (NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendDist, "index.html"));
+  });
+}
+/**
  * Start server only after DB connects
  */
 connectDB().then(() => {
